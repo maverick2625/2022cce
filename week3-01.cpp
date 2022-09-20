cf7e8986cfd29bdbaadfe18c775b0e1ce3fefeef
銀玥 <112918579+maverick2625@users.noreply.github.com>
2022-09-20 11:57:27 +0800

    week3

---
+++ week3-01.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+        int a,b;
+        printf("請輸入兩個數字: ");
+        scanf("%d%d" , &a, &b);
+
+        printf("%d + %d 是 %d\n" , a,b,a+b);
+        printf("%d - %d 是 %d\n" , a,b,a-b);
+        printf("%d * %d 是 %d\n" , a,b,a*b);
+        printf("%d / %d 是 %d\n" , a,b,a/b);
+        printf("%d 除法 %d 的除數是 %d\n" , a,b,a%b);
+}
